d0469f9eceaac14183f1e7a15b0946dfe4c0a500
Ana-Maria-O <38377957+Ana-Maria-O@users.noreply.github.com>
2022-02-22 10:35:59 +0100

    Add files via upload

---
+++ key features.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+#number of replies from one user to another
+
+# in degree
+
+# out degree
+
+# total score
+
+# average score
+
+# controversiality
+
+# sorted list of subreddits active on
+
+# distribution of subreddits active on
+
+# most used words
+
+# tree depth & width
+
+# average score of posts/comments per subreddit
+
+# distribution of posts/comments per subreddit
+
+# most active users per subreddi
+
