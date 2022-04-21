c5116527f59d3729f25cbabac78e78cbb866acac
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2022-04-21 11:01:57 +0200

    Deleted dummy files. Implemented more of the community detection algorithm

--- Community_Detection.py
+++ Community_Detection.py
@@ -1,8 +1,13 @@
+from lib2to3.pytree import Node
 import pickle
+from re import sub
 import networkx as nx
 import matplotlib.pyplot as plt
 from community import community_louvain
 from userNetworkFunction import Network, User
+
+alpha = 1
+sgraph_nodes = {}
 
 def PullOutChanges(graph: nx.classes.graph.Graph, graph_new: nx.classes.graph.Graph) -> list:
     changes = []
@@ -22,25 +27,127 @@ def PullOutChanges(graph: nx.classes.graph.Graph, graph_new: nx.classes.graph.Gr
 
     for edge in graph_new.edges:
         if not graph.has_edge(edge):
-            changes.append({'type': 'add_edge', 'node': edge[0], 'node2': edge[1]})
+            changes.append({'type': 'add edge', 'node': edge[0], 'node2': edge[1]})
     
     return changes
 
-def ConstructCompressedGraph(graph, graph_new, communities) -> nx.classes.graph.Graph:
+def AddSuperEdges(graph: nx.classes.graph.Graph, supergraph: nx.classes.graph.Graph) -> nx.classes.graph.Graph:
+    edges = []
+
+    # for supernode in supergraph.nodes:
+        # for node in supernode
+
+def RemoveNode(node: str, communities: list, graph: nx.classes.graph.Graph, supergraph: nx.classes.graph.Graph):
+    global sgraph_nodes
+
+    # if this node was already accounted for in the supergraph ir its community was already broken, don't do this function again
+    if node in sgraph_nodes or (communities(node), True) in sgraph_nodes.values():
+        return supergraph
+    else:
+        if not(node in sgraph_nodes):
+            sgraph_nodes[node] = (communities[node], False)
+
+    # get degree of the node
+    deg = graph.degree(node)
+
+    # list of nodes in the community of node
+    community = [key for key in communities if communities[key] == communities[node]]
+
+    # sum of degrees in the community
+    comm_deg_sum = 0
+    for item in community:
+        comm_deg_sum += graph.degree(item)
+    
+    # average degree in community
+    deg_c = comm_deg_sum / len(community)
+
+    if deg >= alpha * deg_c:
+        # update sgraph_nodes that this community was broken
+        og_node = sgraph_nodes.keys()[sgraph_nodes.values().index((communities[node], False))]
+        sgraph_nodes[og_node] = (communities[node], True)
+        
+        # if the whole community was already in the supergraph, remove it and re-update the label for every following node
+        found = False
+        for nodes in list(supergraph.nodes(data="nodes")):
+
+            if not found:
+                if node in nodes[1]:
+                    supergraph.remove_node(nodes[0])
+                    found = True
+            else:
+                nx.relabel_nodes(supergraph, {nodes[0]: nodes[0] - 1})
+
+        # get the subcommunities in community
+        subcom = Louvain(graph.subgraph(community))
+        subcom_values = list(subcom.values())
+
+        # add each subcommunity(supernode) to an integer
+        for item in subcom_values:
+            # get all nodes from this subcommunity
+            c_list = [key for key in subcom if subcom[key] == item]
+
+            # link the nodes from the subcommunity to an integer
+            key = supergraph.number_of_nodes()
+            supergraph.add_node(key, nodes=c_list)
+    else:
+        key = supergraph.number_of_nodes()
+        supergraph.add_node(key, nodes=community)
+
+    return supergraph
+
+
+# TODO: add superedges function -> joi
+# TODO: test everything -> vineri + sambata
+# TODO: write code in main -> duminica
+def ConstructCompressedGraph(graph: nx.classes.graph.Graph, graph_new: nx.classes.graph.Graph, communities: dict) -> nx.classes.graph.Graph:
     delta_G = PullOutChanges(graph, graph_new)
+    supergraph = nx.Graph()
     
     for change in delta_G:
         if change['type'] == 'remove node':
             u = change['node']
-            #TODO: replace communities[u] with a subset of nodes in u's community
-            super_nodes, super_edges = RemoveNode(u, communities[u])
+            supergraph = RemoveNode(u, communities, graph, supergraph)
         
         if change['type'] == 'remove edge':
             u = change['node']
             v = change['node2']
 
             if communities[u] == communities[v]:
+                supergraph = RemoveNode(u, communities, graph, supergraph)
+                supergraph = RemoveNode(v, communities, graph, supergraph)
+        
+        if change['type'] == 'add node':
+            u = change['node']
+            supergraph.add_node(supergraph.number_of_nodes(), [u])
 
+            for neighbor in graph_new.neighbors(u):
+                if neighbor in communities:
+                    supergraph = RemoveNode(neighbor, communities, graph, supergraph)
+
+        # TODO: make sure this doesn't add supernodes that include a node which is already in a supernode
+        if change['type'] == 'add edge':
+            u = change['node']
+            v = change['node2']
+
+            n = supergraph.number_of_nodes()
+            in_sg = False
+
+            for node in supergraph.nodes(data="nodes") and not in_sg:
+                if u in node[1] or v in node[1]:
+                    in_sg = True
+
+            if not in_sg:
+                supergraph.add_edge((n, [u]), (n + 1, [v]), graph_new[u][v]["weight"])
+
+            if u in communities and v in communities and communities[u] != communities[v]:
+                supergraph = RemoveNode(u, communities[u], graph, supergraph)
+                supergraph = RemoveNode(v, communities[v], graph, supergraph)
+
+    
+    supergraph = AddSuperEdges(graph, supergraph)
+
+    return supergraph
+            
 
 def Louvain(graph = nx.classes.graph.Graph) -> dict:
     G = community_louvain.best_partition(graph)
@@ -74,10 +181,31 @@ def makeGraph(subreddit = str) -> nx.classes.graph.Graph:
     return G
 
 def main():
-    subreddit = 'lockdownskepticism'
-    G = makeGraph(subreddit)
+    # subreddit = 'lockdownskepticism'
+    # G = makeGraph(subreddit)
+    # print(G.nodes(data="weight"))
+    G = nx.Graph()
+    G.add_node(1, dt = [])
+    l = list(G.nodes(data="dt"))
+    # print(l[0][0])
+    G.remove_node(l[0][0])
+    print(G.nodes)
+    # communities = Louvain(G)
+    #print(Louvain(G))
     # print(G.edges)
+    # for node in G.nodes:
+    #     print(node)
+        # break
 
+    # print(G.degree)
+    # community = [key for key in communities if communities[key] == 2]
+    # sub = Louvain(G.subgraph(community))
+    # print(sub)
+
+    # a = {5: "a"}
+    # b = {5: "b"}
+    # a.update(b)
+    # print(a)
     '''test = nx.Graph()
     test.add_edge('a', 'b')
     for edge in test.edges:
