df8d5741d3de102f8d59c8c33e89d7bdc0b0a76f
Maurits <100292428+mgjfl@users.noreply.github.com>
2022-06-04 10:33:58 +0200

    Sandwiches

--- pushshift_key_features_PCN.py
+++ pushshift_key_features_PCN.py
@@ -193,6 +193,52 @@ def allUsers(subs):
 
     return users
 
+def countSandwiches(node):
+
+    sandwhichCounter = 0
+    possibleSandwiches = 0
+
+    if len(node.next) != 0:
+        for child in node.next:
+            for grandChild in child.next:
+
+                possibleSandwiches += 1
+
+                # Check if sandwich occurs
+                if grandChild.auth == node.auth:
+                    sandwhichCounter += 1
+
+    return sandwhichCounter, possibleSandwiches
+
+def countTotalSandwiches(root):
+    totalSandwhichCounter, totalPossibleSandwiches = countSandwiches(root)
+
+    for child in root.next:
+        temp = countTotalSandwiches(child)
+        totalSandwhichCounter += temp[0]
+        totalPossibleSandwiches += temp[1]
+
+
+    return totalSandwhichCounter, totalPossibleSandwiches
+
+
+def sandwiches(forest):
+    # forestDict[subreddit][post]
+    sandwichDictionary = {sub: dict() for sub in subs}
+
+    for sub in subs:
+        for post in forest[sub].keys():
+            root = forest[sub][post]
+            temp = countTotalSandwiches(root)
+
+            sandwichDictionary[sub][post] = dict()
+            sandwichDictionary[sub][post]["counted"] = temp[0]
+            sandwichDictionary[sub][post]["possible"] = temp[1]
+
+
+
+    return sandwichDictionary
+
 print("Start setup ........")
 print("Start importing posts ........")
 
@@ -236,41 +282,55 @@ sub = posts.keys()
 
 setup(time, posts, psts, comments, users, forest)
 
-print("Replies between users started............")
-# number of replies from one user to another
-replies = repliesBetweenUsers(sub)
-print("Replies between users done!")
+# print("Replies between users started............")
+# # number of replies from one user to another
+# replies = repliesBetweenUsers(sub)
+# print("Replies between users done!")
 #print(replies)
 
-print("In degree started............")
-# in degree
-in_degree = in_out_Degree(sub, replies, True)
-print("In degree done!")
-#print(in_degree)
+print("Sandwiches between users started............")
+sandwichesDict = sandwiches(forest)
 
-print("Out degree started............")
-# out degree
-out_degree = in_out_Degree(sub, replies, False)
-print("Out degree done!")
-#print(out_degree)
+# Convert to decimal values for plotting
+sandwichesDictValues = {s: dict() for s in subs}
+for s in subs:
+    for post in sandwichesDict[s].keys():
+        if sandwichesDict[s][post]["possible"] == 0:
+            sandwichesDictValues[s][post] = None
+        else:
+            sandwichesDictValues[s][post] = sandwichesDict[s][post]["counted"]/sandwichesDict[s][post]["possible"]
 
-print("Total scores started............")
-# total scores
-n_posts, n_comments, t_score_posts, t_score_comments = totalScore(sub)
-print("Total scores done!")
-#print(t_score)
-#print(n_comments)
+print("Sandwiches between users done!")
 
-print("Average scores started............")
-# average score
-a_score_posts, a_score_comments = averageScore(t_score_posts, t_score_comments, n_posts, n_comments, sub)
-print("Average scores done!")
-#print(a_score)
+# print("In degree started............")
+# # in degree
+# in_degree = in_out_Degree(sub, replies, True)
+# print("In degree done!")
+# #print(in_degree)
 
-print("Controversiality started............")
-# controversiality
-p_con, c_con = controversiality(sub)
-print("Controversiality done!")
+# print("Out degree started............")
+# # out degree
+# out_degree = in_out_Degree(sub, replies, False)
+# print("Out degree done!")
+# #print(out_degree)
+
+# print("Total scores started............")
+# # total scores
+# n_posts, n_comments, t_score_posts, t_score_comments = totalScore(sub)
+# print("Total scores done!")
+# #print(t_score)
+# #print(n_comments)
+
+# print("Average scores started............")
+# # average score
+# a_score_posts, a_score_comments = averageScore(t_score_posts, t_score_comments, n_posts, n_comments, sub)
+# print("Average scores done!")
+# #print(a_score)
+
+# print("Controversiality started............")
+# # controversiality
+# p_con, c_con = controversiality(sub)
+# print("Controversiality done!")
 #print(p_con)
 
 #sorted list of subreddits active on
@@ -284,53 +344,54 @@ depth, width, width_dist, depth_dist = treeDepthWidth(sub)
 #print(depths)
 #print(widths)
 
-print("Post/comment ratio started............")
-# post/comment ratio
-p_c_ratio = ratioPostComment(n_posts, n_comments, sub)
-print("Post/comment ratio done!")
-#print(p_c_ratio)
+# print("Post/comment ratio started............")
+# # post/comment ratio
+# p_c_ratio = ratioPostComment(n_posts, n_comments, sub)
+# print("Post/comment ratio done!")
+# #print(p_c_ratio)
 
-# number of levels in post trees between users' replies
-nodes = nodesBetweenReplies(sub)
-#print(nodes)
+# # number of levels in post trees between users' replies
+# nodes = nodesBetweenReplies(sub)
+# #print(nodes)
 
-print(posts, type(posts))
-print()
-print(sub, type(sub))
+# print(posts, type(posts))
+# print()
+# print(sub, type(sub))
 
-# average score of posts/comments per subreddit
-averagescore = [(su, score_metrics(posts[su])[0]) for su in sub]
-print('when the average is scored')
+# # average score of posts/comments per subreddit
+# averagescore = [(su, score_metrics(posts[su])[0]) for su in sub]
+# print('when the average is scored')
 
-# distribution of posts/comments per subreddit
-# averagedist = [activity_distribution(posts[su]) for su in sub]
-print('distributed the posts/comments')
-# most active users per subreddit
-aclist = active_users(forest)
-print('active listed')
+# # distribution of posts/comments per subreddit
+# # averagedist = [activity_distribution(posts[su]) for su in sub]
+# print('distributed the posts/comments')
+# # most active users per subreddit
+# aclist = active_users(forest)
+# print('active listed')
 
 kfDict = {}
 
-kfDict['replies between users'] = replies
-kfDict['in-degree'] = in_degree
-kfDict['out-degree'] = out_degree
-kfDict['nrPosts, nrComments, totalScorePosts, totalScoreComments'] = n_posts, n_comments, t_score_posts, t_score_comments
-kfDict['avgScorePosts, avgScoreComments'] = a_score_posts, a_score_comments
-kfDict['postControversiality, commentControversiality'] = p_con, c_con
+kfDict['sandwiches'] = sandwichesDict
+kfDict['sandwichesValues'] = sandwichesDictValues
+# kfDict['replies between users'] = replies
+# kfDict['in-degree'] = in_degree
+# kfDict['out-degree'] = out_degree
+# kfDict['nrPosts, nrComments, totalScorePosts, totalScoreComments'] = n_posts, n_comments, t_score_posts, t_score_comments
+# kfDict['avgScorePosts, avgScoreComments'] = a_score_posts, a_score_comments
+# kfDict['postControversiality, commentControversiality'] = p_con, c_con
 kfDict['tree depth'] = depth
 kfDict['tree width'] = width
 kfDict['tree depth dist'] = depth_dist
 kfDict['tree width dist'] = width_dist
-kfDict['postCommentRatio'] = p_c_ratio
-kfDict["nr levels in post trees between users' replies"] = nodes
-kfDict['average post/comment score'] = averagescore
-kfDict['most active users per subreddit'] = aclist
+# kfDict['postCommentRatio'] = p_c_ratio
+# kfDict["nr levels in post trees between users' replies"] = nodes
+# kfDict['average post/comment score'] = averagescore
+# kfDict['most active users per subreddit'] = aclist
 
 resultsPath = f"Results/Pushshift/"
 createDirectory(resultsPath)
 
 print(list(kfDict.keys()))
-print(kfDict['average post/comment score'])
 
-with open(resultsPath + f'key_features_pushshift_dist.pickle', 'wb') as f:
+with open(resultsPath + f'KFs_PCN_GroundTruth.pickle', 'wb') as f:
     pickle.dump(kfDict, f, protocol=pickle.HIGHEST_PROTOCOL)
